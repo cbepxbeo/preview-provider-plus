ac2cfe8dd42420b163f72466e2454603eca2b6a7
cbepxbeo <cbepxbeo@mail.ru>
2023-09-17 16:51:16 +0300

    Create PreviewDevicePlus.swift

---
+++ Sources/iOS/Protocols/PreviewDeviceOption/PreviewDevicePlus.swift
@@ -0,0 +1,14 @@
+/*
+ 
+ Project: PreviewProviderPlus
+ File: PreviewDeviceOption.swift
+ Created by: Egor Boyko
+ Date: 17.09.2023
+ 
+ Status: #In progress
+ 
+ */
+
+import SwiftUI
+
+public protocol PreviewDevice {}
