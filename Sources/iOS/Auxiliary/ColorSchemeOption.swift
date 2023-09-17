a254601ed4fcbf35b6189a3e84d0a1e8cf693f1b
cbepxbeo <cbepxbeo@mail.ru>
2023-09-17 16:52:28 +0300

    Create ColorSchemeOption.swift

---
+++ Sources/iOS/Auxiliary/ColorSchemeOption.swift
@@ -0,0 +1,14 @@
+/*
+ 
+ Project: PreviewProviderPlus
+ File: ColorSchemeOption.swift
+ Created by: Egor Boyko
+ Date: 17.09.2023
+ 
+ Status: #In progress
+ 
+ */
+
+import SwiftUI
+
+enum ColorSchemeOption {}
