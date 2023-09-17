8fac4602cc5c51cc62e6ae13151a57d73a4f08a1
cbepxbeo <cbepxbeo@mail.ru>
2023-09-17 16:52:35 +0300

    Create PreviewProvider+StaticMethods+Сomposition..swift

---
+++ Sources/iOS/Extensions/PreviewProvider/PreviewProvider+StaticMethods+Сomposition..swift
@@ -0,0 +1,14 @@
+/*
+ 
+ Project: PreviewProviderPlus
+ File: PreviewProvider+StaticMethods+Сomposition.swift
+ Created by: Egor Boyko
+ Date: 17.09.2023
+ 
+ Status: #In progress
+ 
+ */
+
+import SwiftUI
+
+extension PreviewProvider {}
